21aa9e448b8b841662144b65e5c281de8f02fb8f
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-04-08 19:53:27 +0530

    Remove extra lines of code

--- ReadData.py
+++ ReadData.py
@@ -1,6 +1,5 @@
 import os
 import pathlib
-from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 
 
 def readDataFileMethod(dir_path):
@@ -15,31 +14,16 @@ def readDataFileMethod(dir_path):
     return corpus
 
 
-def readDataPlaintextCorpusReader(dir_path):
-    '''Use built-in feature of PlaintextCorpusReader to read corpus'''
-    corpus = PlaintextCorpusReader(dir_path, '.*')
-    return corpus
-
-
 def main():
     pwd_path = pathlib.Path(__file__).parent.absolute()
     dir_path = str(pwd_path) + '/data/review_data/train/pos'
     
-    # print('\n\nUsing File Method:')
-    # corpus = readDataFileMethod(dir_path)
-    # print(corpus)
-    # for i, x in enumerate(corpus):
-    #     print(f'For File index {i}, the corpus length is {len(x)}')
-    
-
-    print('\n\nUsing PlaintextCorpusReader:')
-    corpus = readDataPlaintextCorpusReader(dir_path)
+    print('\n\nUsing File Method:')
+    corpus = readDataFileMethod(dir_path)
     print(corpus)
-    # files = os.listdir(dir_path)
-    # for i, filename in enumerate(files):
-    #     print(f'For File index {i}, the corpus length is {len(corpus.words(filename))}')
+    for i, x in enumerate(corpus):
+        print(f'For File index {i}, the corpus length is {len(x)}')
     
-
 
 if __name__ == '__main__':
     main()

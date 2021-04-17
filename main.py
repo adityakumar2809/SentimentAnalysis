e62dd4dbdd870f64b2ec7a0659fcf6fd36df9907
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-04-17 23:11:54 +0530

    Make Vectorizers for sample data

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from ReadData import readDataFileMethod
 def getCorpus():
     pwd_path = pathlib.Path(__file__).parent.absolute()
 
-    dir_path = str(pwd_path) + '/data/review_data/train/pos'
+    dir_path = str(pwd_path) + '/data/review_data/train/sample/pos'
     corpus_pos = readDataFileMethod(dir_path)
     pos_length = len(corpus_pos)
     print('Length of POS:', pos_length)
 
     
-    dir_path = str(pwd_path) + '/data/review_data/train/neg'
+    dir_path = str(pwd_path) + '/data/review_data/train/sample/neg'
     corpus_neg = readDataFileMethod(dir_path)
     neg_length = len(corpus_neg)
     print('Length of NEG:', neg_length)
@@ -28,7 +28,7 @@ def saveVectorizerAndVectorizedData(corpus):
 
     saveDataAsCSV(
         vectorizer.get_feature_names(),
-        'result/vectorized_data/final/FeatureList.csv',
+        'result/vectorized_data/sample/FeatureList.csv',
         columns=['Word']
     )
 
@@ -50,9 +50,9 @@ def saveVectorizerAndVectorizedData(corpus):
     #     columns=vectorizer.get_feature_names()
     # )
 
-    filepath = 'result/vectorized_data/final/CountVectorizedData.pkl'
+    filepath = 'result/vectorized_data/sample/CountVectorizedData.pkl'
     saveDataInPickleFile(vectorized_data, filepath)
-    filepath = 'result/vectorizer/final/CountVectorizer.pkl'
+    filepath = 'result/vectorizer/sample/CountVectorizer.pkl'
     saveDataInPickleFile(vectorizer, filepath)
 
 
@@ -75,18 +75,19 @@ def saveVectorizerAndVectorizedData(corpus):
     #     'result/vectorized_data/final/TFIDFVectorized.csv',
     #     columns=vectorizer.get_feature_names()
     # )
-    filepath = 'result/vectorized_data/final/TFIDFVectorizedData.pkl'
+    filepath = 'result/vectorized_data/sample/TFIDFVectorizedData.pkl'
     saveDataInPickleFile(vectorized_data, filepath)
-    filepath = 'result/vectorizer/final/TFIDFVectorizer.pkl'
+    filepath = 'result/vectorizer/sample/TFIDFVectorizer.pkl'
     saveDataInPickleFile(vectorizer, filepath)
 
 
-    filepath = 'result/vectorized_data/final/CountVectorizedData.pkl'
+    filepath = 'result/vectorized_data/sample/CountVectorizedData.pkl'
     vectorized_data = retrieveDataFromPickleFile(filepath)
     print(vectorized_data)
 
 
 def main():
+    print('Initiating process...')
     corpus = getCorpus()
     saveVectorizerAndVectorizedData(corpus)
     
